e17f400ac201ba452cd9e57fc0656b32ef6956d3
joabe <joalokk@gmail.com>
2024-04-01 08:59:38 -0300

    tralala

---
+++ douglao/GamePage.xaml.cs
@@ -0,0 +1,13 @@
+namespace douglao;
+
+public partial class GamePage : ContentPage
+{
+
+	public GamePage()
+	{
+		InitializeComponent();
+	}
+
+
+}
+
--- douglao/MainPage.xaml.cs
+++ douglao/MainPage.xaml.cs
@@ -8,6 +8,21 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	 public void IrOutraPage(object sender, EventArgs args)
+    {
+        Application.Current.MainPage = new GamePage();
+    }
+
+	public void FrameON(object sender, EventArgs args)
+	{
+		Oframe.IsVisible = true;
+	}
+
+	public void FrameOFF(object sender, EventArgs args)
+	{
+		Oframe.IsVisible = false;
+	}
+
 
 }
 
